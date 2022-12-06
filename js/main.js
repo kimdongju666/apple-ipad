9069a718f7db5d301e148abab27eff745d357aec
kimdongju666@gmail.com <kimdongju666@naver.com>
2022-12-07 02:00:49 +0900

    Footer 마무리, overflow hidden

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import ipads from '../data/ipads.js'
+import navigations from '../data/navigations.js'
 
 // 장바구니
 const basketStarterEl = document.querySelector('header .basket-starter')
@@ -130,3 +131,32 @@ ipads.forEach(function (ipad) {
 
   itemsEl.append(itemEl)
 })
+
+const navigationsEl = document.querySelector('footer .navigations')
+navigations.forEach(function (nav) {
+  const mapEl = document.createElement('div')
+  mapEl.classList.add('map')
+  
+  let mapList = ""
+  nav.maps.forEach(function (map) {
+    mapList += /* html */ `
+    <li>
+      <a href="${map.url}">${map.name}</a>
+    </li>`
+  })
+
+  mapEl.innerHTML = /* html */ `
+  <h3>
+    <span class="text">${nav.title}</span>
+  </h3>
+  <ul>
+    ${mapList}
+  </ul>
+  `
+
+  navigationsEl.append(mapEl)
+})
+
+const thisYearEl = document.querySelector('span.this-year')
+thisYearEl.textContent = new Date().getFullYear()
+
